a30ac2722ac299dad2fb8ac3e60cb5da6fec0bf5
Joel Smith <joelwsmith123@gmail.com>
2019-02-14 16:36:45 -0700

    Add TemplateContainer

---
+++ src/TemplateContainer/TemplateContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './TemplateContainer.css';
+
+export default class TemplateContainer extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div className='TemplateContainer'>
+
+      </div>
+    )
+  }
+}
