f640f32cea3b99d08be9a863ea93dafb45d317f8
Joel Smith <joelwsmith123@gmail.com>
2019-02-14 18:48:12 -0700

    Add example of element addition to template

--- src/Templates/CardPageTemplate.js
+++ src/Templates/CardPageTemplate.js
@@ -4,12 +4,35 @@ import './Templates.css';
 export default class CardPageTemplate extends Component {
   constructor() {
     super()
+
+    this.state = {
+      headerButtons: 0
+    }
+  }
+
+  addBtnToHeader = (event) => {
+    event.preventDefault()
+    this.setState({ headerButtons: this.state.headerButtons + 1 })
+  }
+
+  renderBtnToHeader = () => {
+    const btnArr = []
+    for (let i = 0; this.state.headerButtons > i; i++) {
+      btnArr.push(<button key={i} className='template-header-btn'></button>)
+    }
+    return btnArr
   }
 
   render() {
     return (
       <div className='CardPageTemplate'>
-        <div className='template-header'></div>
+        <div className='template-header'>
+        <div className='pseudo-btn-space'></div>
+          <i className="far fa-plus-square" onClick={this.addBtnToHeader}></i>
+          {
+            this.renderBtnToHeader()
+          }
+        </div>
         <div className='template-card-container'>
           <div className='template-card'></div>
           <div className='template-card'></div>
