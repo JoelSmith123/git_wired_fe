dc2cf71abdb702a0c535b47aa3caa890bbcf526c
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-16 23:38:02 -0700

    Methods for rendering a project card

---
+++ src/Projects/ProjectCard.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+
+export default class ProjectCard extends Component {
+
+  // --- Project Name ---
+
+  getTitle() {
+    return this.props.project['projectTitle']
+  }
+
+  renderTitle(){
+    return (
+      <div className='ProjectTitle'> { getTitle() } </div>
+    )
+  }
+
+  // --- Repo Name ---
+
+  getRepo() {
+    return this.props.project['repoName']
+  }
+
+  renderRepo(){
+    return (
+      <div className='RepoName'> { getRepo() } </div>
+    )
+  }
+
+  // --- Updated ---
+
+  getUpdated() {
+    return this.props.project['lastUpdated']
+  }
+
+  renderUpdated(){
+    return (
+      <div className='Updated'> { getUpdated() } </div>
+    )
+  }
+
+  // --- Created ---
+
+  getCreated() {
+    return this.props.project['lastCreated']
+  }
+
+  renderCreated(){
+    return (
+      <div className='Created'> { getCreated() } </div>
+    )
+  }
+
+
+  // --- Image ---
+
+  // TO DO - Display image (not MVP)
+
+
+
+  render() {
+    <span class='ProjectCard'>
+      { renderTitle()   }
+      { renderRepo()    }
+      { renderUpdated() }
+      { renderCreated() }
+    <span>
+  }
+
+
+}
