2b9321b6c5c8fecb6dc28090b75160de451d6f55
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-17 00:33:33 -0700

    Add ProjectCard test - all skipped

---
+++ src/Projects/ProjectCard.test.js
@@ -0,0 +1,90 @@
+const chai   = require('chai');
+const should = chai.should;
+const expect = chai.expect;
+
+import ProjectCard from './ProjectCard.js'
+
+
+const stubProject = {
+    'projectTitle': 'Title1',
+    'repoName':     'RepoName1',
+    'createdAt':    'Created1',
+    'updatedAt':    'Updated1',
+  }
+
+describe('ProjectCard', () => {
+
+  describe('Title', () => {
+    it.skip('gets project title', done => {
+
+      // TO DO - TEST HERE
+
+      // let card = new ProjectCard
+      // card.props.project = stubProject
+      // let card  = <ProjectCard project={stubProject} />
+      // let title = card.getTitle()
+      // expect(title).to.equal('Title1')
+      done();
+    });
+    it.skip('renders project title', done => {
+
+            // TO DO - TEST HERE
+
+      done();
+    });
+  });
+
+  describe('Repo Name', () => {
+    it.skip('gets repo name', done => {
+
+      // TO DO - TEST HERE
+
+      done();
+    });
+    it.skip('renders repo name', done => {
+
+            // TO DO - TEST HERE
+
+      done();
+    });
+  });
+
+  describe('Created At', () => {
+    it.skip('gets created at', done => {
+
+      // TO DO - TEST HERE
+
+      done();
+    });
+    it.skip('renders created at', done => {
+
+            // TO DO - TEST HERE
+
+      done();
+    });
+  });
+
+  describe('Updated At', () => {
+    it.skip('gets updated at', done => {
+
+      // TO DO - TEST HERE
+
+      done();
+    });
+    it.skip('renders updated at', done => {
+
+            // TO DO - TEST HERE
+
+      done();
+    });
+  });
+
+  it('renders card', done => {
+
+    // TO DO - TEST HERE
+
+    done();
+  });
+
+
+});
