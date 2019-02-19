02fa2eb41241106305f4d46d8a01452fa23c2cf1
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-18 21:01:01 -0700

    Move API call from GithubCardContainer to WireframeEditView

--- src/GithubCards/GithubCardContainer.js
+++ src/GithubCards/GithubCardContainer.js
@@ -2,14 +2,14 @@
 import React, { Component } from 'react';
 import GithubCard from './GithubCard.js'
 
-import UserSession from '../tools/UserSession.js'
+// import UserSession from '../tools/UserSession.js'
 
 
 export default class GithubCardContainer {
 
-  constructor = () => {
-    this.user = new UserSession
-  }
+  // constructor = () => {
+  //   this.user = new UserSession
+  // }
 
   // TO DO - we need a way to pass the project ID to this view and the fetch call
 
@@ -18,27 +18,29 @@ export default class GithubCardContainer {
   //  -- make this call outside this class
   //  -- pass this class the projects portion
   //  -- replace methods using the data object with props.cards
-  getProjects() {
-    // TO DO - Confirm ENDPOINT
-    let url = 'https://git-wired-be.herokuapp.com/api/v1/projects/_____/cards'
-    fetch(url, {
-        method:  "GET",
-        headers: { "Content-Type": "application/json", },
-        body:    JSON.stringify(this.user.getGitWiredToken()),
-    })
-    .then(response => response.json() )
-    .then(data     => this.renderGithubCards(data).bind(this))
-    .catch(error   => console.error('Error:', error));
-  }
+  // getProjects() {
+  //   // TO DO - Confirm ENDPOINT
+  //   let url = 'https://git-wired-be.herokuapp.com/api/v1/projects/_____/cards'
+  //   fetch(url, {
+  //       method:  "GET",
+  //       headers: { "Content-Type": "application/json", },
+  //       body:    JSON.stringify(this.user.getGitWiredToken()),
+  //   })
+  //   .then(response => response.json() )
+  //   .then(data     => this.renderGithubCards(data).bind(this))
+  //   .catch(error   => console.error('Error:', error));
+  // }
 
-  cards = (data) => {
-    return data['cards']
-  }
+  // cards = (data) => {
+  //   return data['cards']
+  // }
 
 
   // TO DO - TEST ME
-  renderGithubCards = (data) => {
-    let cards = this.cards(data)
+  // renderGithubCards = (data) => {
+  renderGithubCards = () => {
+    // let cards = this.cards(data)
+    let cards = this.props.cards
     let l = cards.length
     for(let i = 0; i < l; i++) {
       let card = cards[i]
