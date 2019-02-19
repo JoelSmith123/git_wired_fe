4b731c2c4f0cbc4442003eb1a27510cbea957632
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-18 20:03:01 -0700

    Identify a method that needs to be moved to another class and steps to replace functionality

--- src/GithubCards/GithubCardContainer.js
+++ src/GithubCards/GithubCardContainer.js
@@ -13,6 +13,11 @@ export default class GithubCardContainer {
 
   // TO DO - we need a way to pass the project ID to this view and the fetch call
 
+  // TO DO - This is not where to make this call, because this call will also
+  // get the outer page information (repo & link, project & link, timestamps)
+  //  -- make this call outside this class
+  //  -- pass this class the projects portion
+  //  -- replace methods using the data object with props.cards
   getProjects() {
     // TO DO - Confirm ENDPOINT
     let url = 'https://git-wired-be.herokuapp.com/api/v1/projects/_____/cards'
