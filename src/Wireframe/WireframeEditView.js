11cca7d2a6e688b69986eb825dbcbf3af2783d78
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-19 00:11:45 -0700

    Remove API calls, now in WireframeService

--- src/Wireframe/WireframeEditView.js
+++ src/Wireframe/WireframeEditView.js
@@ -1,21 +1,28 @@
 
+import React, { Component } from 'react';
 
-export default class WireframeEditView {
+import TemplateContainer   from '../TemplateContainer/TemplateContainer.js'
+import GithubCardContainer from '../GithubCards/GithubCardContainer.js'
+import WireframeService    from './WireframeService.js'
 
-  constructor = () => {
-    this.user = new UserSession
 
-    // this.getGithub()
-    // this.getWireframe()
+export default class WireframeEditView extends Component {
+
+
+  componentDidMount() {
+    const frame = new WireframeService
+    // ------- API call HERE ----------
+    // TO DO - add this when API is functional
+    // frame.getGithub( parseFunc = (data) => { this.parseGithubResponse(data) })
+    this.parseGithubResponse( frame.stubGithub() )
+    // ------- API call HERE ----------
+    // TO DO - add this when API is functional
+    // frame.getWireframe( parseFunc = (data) => { this.parseWireframeResponse(data) })
+    this.parseWireframeResponse( frame.stubWireframe() )
   }
 
 
-  getWireframe = () => {
-    // HOW DOES ENDPOINT WORK
-    //
-    //
-    // .then(data     => this.parseWireframeResponse(data))
-  }
+  //  --- Make Wireframe API req ---
 
   parseWireframeResponse = (data) => {
     let info    = data['data']['attributes']
@@ -29,27 +36,13 @@ export default class WireframeEditView {
     } )
   }
 
-  getGithub = () => {
-    // TO DO - Confirm ENDPOINT
-    let id  = this.props.project_id
-    let url = `https://git-wired-be.herokuapp.com/api/v1/projects/${id}/cards`
-    fetch(url, {
-        method:  "GET",
-        headers: { "Content-Type": "application/json", },
-        body:    JSON.stringify(this.user.getGitWiredToken()),
-    })
-    .then(response => response.json() )
-    // .then(data     => this.renderGithubCards(data).bind(this))
-    // .then(data     => this.parseResponse(data).bind(this))
-    .then(data     => this.parseGithubResponse(data))
-    .catch(error   => console.error('Error:', error));
-  }
+  //  --- Make GITHUB API req ---
 
   parseGithubResponse = (data) => {
     let info    = data['data']['attributes']
     let repo    = info['repo']
     let project = info['project']
-    let cards   = info['card']
+    let cards   = info['cards']
     this.setState( {
       repo:    repo,
       project: project,
@@ -59,36 +52,37 @@ export default class WireframeEditView {
 
 
   render() {
+    if (this.state == null || undefined) { return null }
     return (
       <div className='WireframeEditView'>
         <div className='WireframeHeaderContainer'>
-          // Come from wireframe API
+
           <span className='WireframeTitle'>{ this.state.wireframeTitle }</span>
           <span className='Timestamps'>
             <span className='UpdatedTimestamp'>{ this.state.wireframeUpdated }</span>
             <span className='CreatedTimestamp'>{ this.state.wireframeCreated }</span>
           </span>
-          // Come from cards API
+
           <span className='Timestamps'>
             <span className='RepoName'>
-              <a href={this.state.repo.url}>{ this.state.repo.name }</a>
+              <a href={this.state.repo['url']}>{ this.state.repo['name'] }</a>
             </span>
             <span className='ProjectName'>
-              <a href={this.state.project.url}>{ this.state.wireframeCreated }</a>
+              <a href={this.state.project['url']}>{ this.state.project['name'] }</a>
             </span>
           </span>
+
         </div>
-        // TO DO - we need to make sure template gets passed here
-        //  -- props vs state ?
+
         <div className='WireframeEditContent'>
-          // Can we confirm that these are loaded in the correct order when both make async calls?
-          <TemplateContainer currentTemplate={this.state.template} />
-          <GithubCardContainer cards={ this.state.cards} />
+          <TemplateContainer currentTemplate={this.props.template} />
+          <GithubCardContainer cards={ this.state.cards } />
         </div>
 
       </div>
     )
   }
+
 
 
 
