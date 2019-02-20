4828f4cd253727c64a3e76b44109ae87c824cb3b
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-19 20:55:41 -0700

    Allow App method to be invoked as callback

--- src/App/App.js
+++ src/App/App.js
@@ -2,19 +2,12 @@ import React, { Component } from 'react';
 
 import './App.css';
 
-import Header from '../Header/Header.js';
-import TemplateContainer from '../TemplateContainer/TemplateContainer.js'
-// import RecentProjects from '../Projects/RecentProjects.js'
-
-// ---- TO DO - DELETE THIS PORTION ----
-// Only for demonstrating functionality while BE API is not built
-// import ProjectCard        from '../Projects/ProjectCard.js'
-// import GithubCard         from '../GithubCards/GithubCard.js'
+import Header             from '../Header/Header.js';
+import Welcome            from '../Welcome/Welcome.js'
+import Profile            from '../Profile/Profile.js';
 import RecentProjects     from '../Projects/RecentProjects.js'
 import WireframeEditView  from '../Wireframe/WireframeEditView.js'
-// --------------------------------------
-import Profile from '../Profile/Profile.js';
-import Welcome from '../Welcome/Welcome.js'
+import TemplateContainer  from '../TemplateContainer/TemplateContainer.js'
 
 import './App.css';
 
@@ -24,22 +17,14 @@ export default class App extends Component {
     this.state = {
       loggedIn: false,
       template: '',
-      // This attribute is temporary until API is functional
-      // stubCard: {
-      //       'projectTitle': 'Title1',
-      //       'repoName':     'RepoName1',
-      //       'createdAt':    'Created1',
-      //       'updatedAt':    'Updated1',
-      //   },
-      //   stubGithubCard: {
-      //     'cardTitle':       'This is the title',
-      //     'cardDescription': 'This is lots of long text from the body of my issue',
-      //     'cardNumber':       13,
-      //     'cardURL':         'https://github.com/JoelSmith123/git_wired_fe/issues/23',
-      //     'cardStatus':      'Open',
-      //     'cardColumn':      'Custom Column',
-      //   },
       viewProfile: false
+    }
+  }
+
+  componentDidMount() {
+    if (Boolean(this.props.user.getURLToken())) {
+      this.props.user.login()
+      this.setState({ loggedIn: this.props.user.isLoggedIn() })
     }
   }
 
@@ -50,9 +35,8 @@ export default class App extends Component {
   }
 
   changeLoggedInState = (event) => {
-    event.preventDefault()
+    if (event) {event.preventDefault()}
 
-    // this.setState({ loggedIn: !this.state.loggedIn })
     this.setState({ loggedIn: this.props.user.isLoggedIn() })
   }
 
@@ -68,6 +52,7 @@ export default class App extends Component {
   selectComponentRender = () => {
     if (this.state.loggedIn) {
       return <WireframeEditView template={this.state.template}/>
+      // return <Profile />
     } else {
       return <Welcome />
     }
@@ -80,6 +65,7 @@ export default class App extends Component {
                 changeLoggedInState={this.changeLoggedInState}
                 selectTemplate={this.selectTemplate}
                 viewProfile={this.viewProfile}
+                user={this.props.user}
         />
         { this.selectComponentRender() }
         <div className='footer'>
