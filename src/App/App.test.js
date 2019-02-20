42a5971d3a0d3b3bfab922810cf953e135dd62ec
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-20 00:24:52 -0700

    Correct App test by passing a new UserSession

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,16 +1,18 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
-import App      from './App';
 
 const chai   = require('chai');
 const should = chai.should;
 const expect = chai.expect;
 
+import App         from './App';
+import UserSession from '../Tools/UserSession.js'
+
 describe('App', () => {
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<App user={new UserSession}/>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
