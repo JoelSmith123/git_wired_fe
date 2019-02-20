d917bc89e0633a5552739e64b2752123a084b6ca
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-19 17:23:08 -0700

    Add session method for determining if user is logged in

--- src/Tools/UserSession.js
+++ src/Tools/UserSession.js
@@ -15,26 +15,33 @@ export default class UserSession {
     sessionStorage.removeItem(this.tokenKey);
   }
 
+
+  isLoggedIn = () => {
+    return Boolean( this.getGitWiredToken() )
+  }
+
+
+
+
   // Make Fetch /login call
   // receive userToken
-
 
   // TO DO - TEST ME
   //  - CONFIRM endpoint
   //  - CONFIRM RESPONSE STRUCTURE
-  loginUser() {
-    let url = 'https://git-wired-be.herokuapp.com/login'
-    fetch(url)
-      .then(response => response.json())
-      .then(data     => this.getToken(data).bind(this))
-      .then(token    => this.setGitWiredToken(token).bind(this))
-      .catch(error   => { console.log(error); });
-  }
-
-  getToken(data) {
-    let token = data[this.tokenKey]
-    return token
-  }
+  // loginUser() {
+  //   let url = 'https://git-wired-be.herokuapp.com/login'
+  //   fetch(url)
+  //     .then(response => response.json())
+  //     .then(data     => this.getToken(data).bind(this))
+  //     .then(token    => this.setGitWiredToken(token).bind(this))
+  //     .catch(error   => { console.log(error); });
+  // }
+  //
+  // getToken(data) {
+  //   let token = data[this.tokenKey]
+  //   return token
+  // }
 
 
 }
