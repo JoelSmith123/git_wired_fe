eb61d8233790ac5f47516cb1ae8a271390799812
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-19 23:57:43 -0700

    Add Mocha / Chai to App.test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React    from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App      from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const chai   = require('chai');
+const should = chai.should;
+const expect = chai.expect;
+
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+
 });
