3247677de24e250d7799dcf74b1de6855efd5b93
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-19 21:17:22 -0700

    Implement a SessionButton that switches login/logout based on sessionStorage

---
+++ src/Header/SessionButton.js
@@ -0,0 +1,52 @@
+
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+
+  constructor(props) {
+    super(props)
+    this.updateState = this.props.changeLoggedInState.bind(this)
+  }
+
+
+ goToAuth = () => {
+   window.location.href='https://git-wired-be.herokuapp.com/api/v1/authenticate'
+ }
+
+ loginButton = () => {
+   return (
+     <input type="button"
+            onClick={ () => {
+              this.props.user.setGitWiredToken('123');
+              this.updateState()
+              }
+            }
+            className='header-btn header-login-btn'
+            value="Login"
+    />
+   )
+ }
+
+ logoutButton = () => {
+   return (
+     <input type="button"
+            onClick={ () => {
+              this.props.user.logout();
+              this.updateState()
+              }
+            }
+            className='header-btn header-logout-btn'
+            value="Logout"
+    />
+   )
+ }
+
+ render() {
+  return (
+    <React.Fragment>
+      { this.props.user.isLoggedIn() ? this.logoutButton() : this.loginButton() }
+    </React.Fragment>
+  )
+ }
+
+}
