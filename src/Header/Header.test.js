080fa6f5a8886677a5789d4636cded66e906bf06
Joel Smith <joelwsmith123@gmail.com>
2019-02-21 04:37:11 -0700

    Add Header test for conditional render based on loggedIn

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -37,4 +37,10 @@ describe('Header', () => {
     expect(wrapper.state('showDropdown')).toEqual(false)
   })
 
+  it('should conditionally render header based on loggedIn state', () => {
+    const wrapper = mount(<Header user={ new UserSession } selectTemplate={jest.fn()} loggedIn={true} changeLoggedInState={jest.fn()}/>)
+
+    expect(wrapper.find('.header-recent-projects-btn-dropdown-container')).toHaveLength(1)
+  })
+
 });
