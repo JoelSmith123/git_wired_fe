db6ce5b3ce70787e2cfcb50ab9f2d04bb7f569a5
Joel Smith <joelwsmith123@gmail.com>
2019-02-21 00:41:27 -0700

    Add BlogPostTemplate test for render buttons to header based on state

--- src/Templates/BlogPostTemplate.test.js
+++ src/Templates/BlogPostTemplate.test.js
@@ -25,4 +25,12 @@ describe('BlogPostTemplate', () => {
 
     expect(wrapper.state('headerButtons')).toEqual(2)
   })
+
+  it('should render new buttons based on state', () => {
+    const wrapper = shallow(<BlogPostTemplate />)
+    const mockHeaderButtonArr = "[{\"type\":\"button\",\"key\":\"0\",\"ref\":null,\"props\":{\"className\":\"template-header-btn\"},\"_owner\":null,\"_store\":{}}]"
+    wrapper.setState({ headerButtons: 1 })
+
+    expect(JSON.stringify(wrapper.instance().renderBtnToHeader())).toEqual(mockHeaderButtonArr)
+  })
 })
