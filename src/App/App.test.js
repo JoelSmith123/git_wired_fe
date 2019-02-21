28ab8acb99316c2d06800e1baf8c770fdc227321
Joel Smith <joelwsmith123@gmail.com>
2019-02-20 20:05:34 -0700

    Add beginning tests for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,11 +1,7 @@
-import React    from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-
-const chai   = require('chai');
-const should = chai.should;
-const expect = chai.expect;
-
-import App         from './App';
+import { shallow, mount } from 'enzyme';
+import App from './App';
 import UserSession from '../Tools/UserSession.js'
 
 describe('App', () => {
@@ -16,5 +12,42 @@ describe('App', () => {
     ReactDOM.unmountComponentAtNode(div);
   });
 
+  it('should match snapshot', () => {
+    const wrapper = shallow(<App user={new UserSession}/>)
+    expect(wrapper).toMatchSnapshot()
+  })  
+
+  it('should select template', () => {
+    const wrapper = shallow(<App user={new UserSession}/>)
+    const mockEvent = { target: {}, preventDefault: jest.fn() }
+    const mockTemplate = 'mock-template'
+
+    wrapper.setState({ template: '' })
+    wrapper.instance().selectTemplate(mockEvent, mockTemplate)
+
+    expect(wrapper.state('template')).toEqual(mockTemplate)
+  })
+
+  it('should update logged in state', () => {
+    const mockUser = { isLoggedIn: jest.fn(), getURLToken: jest.fn() }
+    const wrapper = shallow(<App user={mockUser}/>)
+    const mockEvent = { target: {}, preventDefault: jest.fn() }
+
+    wrapper.setState({ loggedIn: false })
+    wrapper.instance().changeLoggedInState(mockEvent)
+
+    expect(mockUser.isLoggedIn).toHaveBeenCalled()
+  })
+
+  it('should update state to view profile', () => {
+    const wrapper = shallow(<App user={new UserSession}/>)
+    const mockEvent = { target: {}, preventDefault: jest.fn() }
+
+    wrapper.setState({ viewProfile: false, template: 'template' })
+    wrapper.instance().viewProfile(mockEvent)
+
+    expect(wrapper.state('viewProfile')).toEqual(true)
+    expect(wrapper.state('template')).toEqual('')
+  })
 
 });
