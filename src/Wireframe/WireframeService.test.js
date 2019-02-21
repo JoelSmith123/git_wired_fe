b2ed4d85a2f36ccc7ff608eec38eaa7007033127
Joel Smith <joelwsmith123@gmail.com>
2019-02-21 01:45:33 -0700

    Add WireframeService test for stubGithub stub return

--- src/Wireframe/WireframeService.test.js
+++ src/Wireframe/WireframeService.test.js
@@ -12,5 +12,29 @@ describe('WireframeService', () => {
   it('should match snapshot', () => {
     const wrapper = wireframeService
     expect(wrapper).toMatchSnapshot()
-  })   
+  }) 
+
+  it('should return stub on stubGithub invocation', () => {
+    const wrapper = wireframeService
+    const mockStubCards = [{
+        'cardTitle':       "Card 1",
+        'cardDescription': "Description 1",
+        'cardURL':         "https://github.com/JoelSmith123/git_wired_fe/issues/23",
+        'cardNumber':      11,
+        'cardStatus':      'open',
+        'cardColumn':      'Column 1',
+      },
+      {
+        'cardTitle':       "Card 2",
+        'cardDescription': "Description 2",
+        'cardURL':         "https://github.com/JoelSmith123/git_wired_fe/issues/23",
+        'cardNumber':      12,
+        'cardStatus':      'closed',
+        'cardColumn':      'Column 2',
+      }
+    ]
+    
+    expect(wrapper.stubGithub().data.attributes.cards[0]).toEqual(mockStubCards[0])
+    expect(wrapper.stubGithub().data.attributes.cards[1]).toEqual(mockStubCards[1])
+  })  
 })
