0c72de0e133bd9cc78088e07338351d90a35f242
Joel Smith <joelwsmith123@gmail.com>
2019-02-21 01:13:35 -0700

    Add WireframeEditView test for parseWireframeResponse

--- src/Wireframe/WireframeEditView.test.js
+++ src/Wireframe/WireframeEditView.test.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme';
 import WireframeEditView from './WireframeEditView.js'
+import WireframeService from './WireframeService.js'
+
 
 describe('WireframeEditView', () => {
+  let frame
+  let mockData
+  beforeEach(() => {
+    frame = new WireframeService
+    mockData = frame.stubWireframe()
+  })
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -15,4 +23,20 @@ describe('WireframeEditView', () => {
     const wrapper = shallow(<WireframeEditView />)
     expect(wrapper).toMatchSnapshot()
   })   
+
+  it('should parse wireframe response', () => {
+    const wrapper = shallow(<WireframeEditView />)
+    const mockInfo = mockData['data']['attributes']
+    const mockState = {
+      wireframeTitle: mockInfo['title'],
+      wireframeUpdated: mockInfo['updated'],
+      wireframeCreated: mockInfo['created']
+    }
+
+    wrapper.instance().parseWireframeResponse(frame.stubWireframe())
+
+    expect(wrapper.state('wireframeTitle')).toEqual(mockState.wireframeTitle)
+    expect(wrapper.state('wireframeUpdated')).toEqual(mockState.wireframeUpdated)
+    expect(wrapper.state('wireframeCreated')).toEqual(mockState.wireframeCreated)
+  })
 })
