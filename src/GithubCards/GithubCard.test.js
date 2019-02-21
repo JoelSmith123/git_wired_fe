273fd30d540fdaf40bb9cc7720235381243b3b6d
Joel Smith <joelwsmith123@gmail.com>
2019-02-20 20:40:23 -0700

    Add GitHubCard test to get title from card

--- src/GithubCards/GithubCard.test.js
+++ src/GithubCards/GithubCard.test.js
@@ -1,36 +1,37 @@
-import React    from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-
-const chai   = require('chai');
-const should = chai.should;
-const expect = chai.expect;
-
+import { shallow, mount } from 'enzyme';
 import GithubCard from './GithubCard.js'
-
-
-var card = new GithubCard
-
-const data = {
-   'cardTitle':       'This is the title',
-   'cardDescription': 'This is lots of long text from the body of my issue',
-   'cardNumber':       13,
-   'cardURL':         'https://github.com/JoelSmith123/git_wired_fe/issues/23',
-   'cardStatus':      'Open',
- }
 
 
 
 describe('GithubCard', () => {
-
+  let mockData
+  beforeEach(() => {
+    mockData = {
+       'cardTitle':       'This is the title',
+       'cardDescription': 'This is lots of long text from the body of my issue',
+       'cardNumber':       13,
+       'cardURL':         'https://github.com/JoelSmith123/git_wired_fe/issues/23',
+       'cardStatus':      'Open',
+     }  
+  })
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<GithubCard card={ data } />, div);
+    ReactDOM.render(<GithubCard card={ mockData } />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  xit('gets title from card', () => {
-    // TO DO - TEST ME
+  it('should match snapshot', () => {
+    const wrapper = shallow(<GithubCard card={ mockData }/>)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('gets title from card', () => {
+    const wrapper = shallow(<GithubCard card={ mockData }/>)
+    
+    expect(wrapper.instance().getTitle()).toEqual(mockData.cardTitle)
   })
 
   xit('gets description from card', () => {
